1b0945fef6a726e0607c5b0ac57e61e8341cf674
Kevin Liang <kevinliang43gmail.com>
2018-05-20 18:27:27 -0400

    Minimal viable product for multiscraper now works. Scrapes for all courses from NEU.

--- source/scrapers/online_scraper.py
+++ source/scrapers/online_scraper.py
@@ -1,11 +1,17 @@
 import bs4
 import urllib
+from base_online_scraper import base_online_scraper as scraper
 
-URL = 'http://catalog.northeastern.edu/course-descriptions/'
+BASE_URL = 'http://catalog.northeastern.edu'
+INITIAL_PATH = '/course-descriptions/'
 
-fp = urllib.urlopen(URL)
+fp = urllib.urlopen(BASE_URL + INITIAL_PATH)
 soup = bs4.BeautifulSoup(fp, 'lxml')
-nav_menu = soup.find("div", {"id": "cl-menu"})
-print nav_menu.text
+nav_menu = soup.find("div", {"id": "atozindex"}).find_all('a', href=True)
 
+scraper = scraper('')
+
+for a in nav_menu:
+    scraper.url = BASE_URL + a['href']
+    scraper.scrape()
 
