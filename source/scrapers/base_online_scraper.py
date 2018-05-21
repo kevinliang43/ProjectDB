c11b9bf28f5b7dceb646c8f2ff46d818f2111b9c
Kevin Liang <kevinliang43gmail.com>
2018-05-20 21:13:51 -0400

    Added documentation to the base_online_scraper file.

--- source/scrapers/base_online_scraper.py
+++ source/scrapers/base_online_scraper.py
@@ -2,17 +2,46 @@ from bs4 import BeautifulSoup
 import urllib
 
 class base_online_scraper(object):
+    """Class Definition for an online scraper.
+    The base_online_scraper scrapes 1 page of course data from a webpage.
+
+    url: a String representing the webpage that this is going to scrape.
+    """
 
     def __init__(self, url):
+        """Inits an instance of base_online_scraper
+        
+        Args:
+            url: A string representing the webpage that is to be scraped.
+
+        Returns:
+            An instance of base_online_scraper with the url as *url*.
+        """
+
         self.url = url
 
     def __setup(self): 
+        """ Sets up the scraper by first opening up the url, parsing the HTML,
+        and then returning a parsed portion of the HTML containing the course
+        information
+
+        Returns:
+            A portion of the HTML that is semi parsed, containing all
+            information about the courses on that particular page.
+            Returned as a list of tags (bs4.element.tag)
+        """
+
         fp = urllib.urlopen(self.url)
         soup = BeautifulSoup(fp, 'lxml')
         course_block = soup.find_all('div', class_="courseblock")
         return course_block
 
     def scrape(self):
+    """Calls the __setup function to obtain all course information on the page,
+    and then parses the data for each courses' individual data, including
+    course code, course name, credits and course description.
+    """
+
         course_block = self.__setup()
 
         for course in course_block:
@@ -28,12 +57,4 @@ class base_online_scraper(object):
             credits.strip(" .")
             courseDesc.strip()
             print courseCode
-
-URL = 'http://catalog.northeastern.edu/course-descriptions/acct/'
-
-ACCT = base_online_scraper(URL)
-
-ACCT.scrape()
-
-
 
